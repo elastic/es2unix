d3b2007aba6a14c5e7df5cc1e0d19ec61a73e59f
Drew Raines <aaraines@gmail.com>
2013-01-10 10:20:45 -0600

    Display usage if no command entered.

--- src/es/main.clj
+++ src/es/main.clj
@@ -5,8 +5,7 @@
 (def opts
   [["-h" "--host" "ES instance hostname" :default "localhost"]
    ["-p" "--port" "ES instance port" :parse-fn #(Integer. %) :default "9200"]
-   ["-v" "--[no-]verbose" :default true]
-   ["-l" "--log-directory" :default "/some/path"]]  )
+   ["-v" "--[no-]verbose" :default true]])
 
 (defn find-command [ns var]
   (let [var (symbol (format "%s/%s" ns var))]
@@ -21,6 +20,11 @@
     (flush))
   (System/exit 99))
 
+(defn help [bann]
+  (println "Usage: es COMMAND [OPTS]")
+  (println)
+  (println (.replace bann "Usage:\n\n" "")))
+
 (defn main [cmd args opts]
   (let [cmd (find-command
              (symbol (format "es.command.%s" cmd))
@@ -30,9 +34,11 @@
       :fail)))
 
 (defn -main [& args]
-  (let [[opts args help] (apply cli args opts)]
+  (let [[opts args banner] (apply cli args opts)]
     (let [[cmd args] args
           res (main cmd args opts)]
       (condp = res
-        :fail (error "no command %s" cmd)
+        :fail (if cmd
+                (error "no command %s" cmd)
+                (help banner))
         (println (.trim res))))))
