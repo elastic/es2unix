e10fc36d5dd23a0cdbcaf43b172870b04e94ddca
Drew Raines <aaraines@gmail.com>
2013-01-14 10:55:13 -0600

    Add client/master/data attrs to nodes.

--- src/es/command/nodes.clj
+++ src/es/command/nodes.clj
@@ -1,7 +1,37 @@
 (ns es.command.nodes
+  (:refer-clojure :exclude [false? true?])
   (:require [es.http :as http]
             [es.command.master :refer [master-id]]
             [es.format.network :refer [parse-addr]]))
+
+(def defaults
+  {:client false
+   :data true
+   :master true})
+
+(defn true? [attr]
+  (= attr "true"))
+
+(defn false? [attr]
+  (not (true? attr)))
+
+(defn true-attr? [attrs attr]
+  (if attrs
+    (if (attrs attr)
+      (true? (attrs attr))
+      (defaults attr))
+    (defaults attr)))
+
+(defn store-data? [node]
+  (true-attr? (:attributes node) :data))
+
+(defn client? [node]
+  (true-attr? (:attributes node) :client))
+
+(defn master-eligible? [node]
+  (if (client? node)
+    false
+    (true-attr? (:attributes node) :master)))
 
 (defn nodes [url]
   (http/get (str url "/_cluster/nodes")))
@@ -11,13 +41,23 @@
         mast (master-id url)]
     (for [[id node] (:nodes res)]
       (let [httpaddr (-> node :http_address parse-addr)
-            tranaddr (-> node :transport_address parse-addr)]
+            tranaddr (-> node :transport_address parse-addr)
+            I-am-master? (= (name id) mast)
+            master? (if (master-eligible? node)
+                      (if I-am-master?
+                        "*"
+                        " ")
+                      "-")
+            data? (if (client? node)
+                    "c"
+                    (if (store-data? node)
+                      "d"
+                      "-"))]
         [(name id)
          (:ip httpaddr)
          (:port httpaddr)
          (:ip tranaddr)
          (:port tranaddr)
-         (if (= (name id) mast)
-           "*"
-           " ")
+         master?
+         data?
          (:name node)]))))
--- test/es/test/command/nodes.clj
+++ test/es/test/command/nodes.clj
@@ -1,14 +1,16 @@
 (ns es.test.command.nodes
-  (:require [clojure.test :refer :all])
-  (:require [es.command.nodes :as nodes]))
+  (:require [clojure.test :refer :all]
+            [es.command.nodes :as nodes]))
 
 (deftest nodes
-  (is (= [["j27iagsmQQaeIpl6yU6mCg" "127.0.0.1"
-           "9203" "127.0.0.1" "9303" " " "Georgianna Castleberry"]
+  (is (= [["Uv1Iy8FvR0y6_RzPXKBolg" "127.0.0.1" "9201"
+           "127.0.0.1" "9300" " " "d" "Cannonball I"]
+          ["j27iagsmQQaeIpl6yU6mCg" "127.0.0.1" "9203"
+           "127.0.0.1" "9303" "-" "c" "Georgianna Castleberry"]
           ["Q4qgMlrETs67mUL-iGHC4A" "127.0.0.1" "9202"
-           "127.0.0.1" "9302" " " "Luichow, Chan"]
+           "127.0.0.1" "9302" " " "d" "Luichow, Chan"]
+          ["AvYWeugwQjqs0cB1vr3D-w" "127.0.0.1" "9204"
+           "127.0.0.1" "9304" "-" "d" "Eric the Red"]
           ["J-erllamTOiW5WoGVUd04A" "127.0.0.1" "9200"
-           "127.0.0.1" "9301" " " "Slade, Frederick"]
-          ["Uv1Iy8FvR0y6_RzPXKBolg" "127.0.0.1" "9201"
-           "127.0.0.1" "9300" " " "Cannonball I"]]
+           "127.0.0.1" "9301" " " "d" "Slade, Frederick"]]
          (nodes/go [] {:url "local:"}))))
