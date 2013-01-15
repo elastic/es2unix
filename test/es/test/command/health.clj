d4199de4663958d7613ea48e196964bd351a49f6
Drew Raines <aaraines@gmail.com>
2013-01-15 15:18:06 -0600

    Remove tests.

--- test/es/test/command/health.clj
+++
@@ -1,7 +0,0 @@
-(ns es.test.command.health
-  (:require [clojure.test :refer :all])
-  (:require [es.command.health :as health]))
-
-(deftest health
-  (is (= [["elasticsearch" "green" 3 3 3 3 0 0 0]]
-         (health/go [] {:url "local:"}))))
