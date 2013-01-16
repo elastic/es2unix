1ce4deeb324424e03d6adb542075c8807929d9db
Drew Raines <aaraines@gmail.com>
2013-01-16 14:39:26 -0600

    Add count command.

---
+++ src/es/command/count.clj
@@ -0,0 +1,16 @@
+(ns es.command.count
+  (:require [es.data.cluster :as cluster]
+            [es.util :as util]))
+
+(defn go [args {:keys [url verbose]}]
+  (let [[q] args
+        res (if q
+              (cluster/count url q)
+              (cluster/count url))
+        failed (or (get-in res [:_shards :failed]) 99999)
+        out (util/commafy (:count res))
+        out (if q (format "%s \"q=%s\"" out q) out)
+        out (if (pos? failed)
+              (format "%s ...with %d failed shards!" out failed)
+              out)]
+    [[out]]))
