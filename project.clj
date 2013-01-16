7ef3fd4c2ccc1b9b156c736a6e96f2bfb4d4310c
Drew Raines <aaraines@gmail.com>
2013-01-16 07:57:17 -0600

    clojure-1.5.0-RC2.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :url "http://elasticsearch.org/"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.5.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.5.0-RC2"]
                  [org.clojure/tools.cli "0.2.2"]
                  [cheshire "5.0.1"]
                  [clj-http "0.6.3"]
