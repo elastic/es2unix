1754518f6c91e48452ffe08fd396665a9300676d
Drew Raines <aaraines@gmail.com>
2013-01-16 13:04:18 -0600

    Shorten health col names.

--- src/es/command/health.clj
+++ src/es/command/health.clj
@@ -2,20 +2,20 @@
   (:require [es.data.cluster :as cluster]))
 
 (def cols
-  [:cluster_name
-   :status
-   :number_of_nodes
-   :number_of_data_nodes
-   :active_primary_shards
-   :active_shards
-   :relocating_shards
-   :initializing_shards
-   :unassigned_shards])
+  [['cluster  :cluster_name]
+   ['status   :status]
+   ['nodes    :number_of_nodes]
+   ['data     :number_of_data_nodes]
+   ['pri      :active_primary_shards]
+   ['shards   :active_shards]
+   ['relo     :relocating_shards]
+   ['init     :initializing_shards]
+   ['unassign :unassigned_shards]])
 
 (defn go [args {:keys [url verbose]}]
   (concat
    (if verbose
-     [(map name cols)])
+     [(map (comp name first) cols)])
    (let [res (cluster/health url)
-         vals (apply juxt cols)]
+         vals (apply juxt (map second cols))]
      [(vals res)])))
