ceffbb6b2355386a2c0ecf5a28006467e38bc792
Drew Raines <aaraines@gmail.com>
2013-01-18 12:14:30 -0600

    Add deep-merge-with.

--- src/es/util.clj
+++ src/es/util.clj
@@ -29,6 +29,22 @@
        (apply concat)
        reverse
        (apply str)))
+
+(defn deep-merge-with
+  "Like merge-with, but merges maps recursively, applying the given fn
+  only when there's a non-map at a particular level.
+
+  (deep-merge-with + {:a {:b {:c 1 :d {:x 1 :y 2}} :e 3} :f 4}
+               {:a {:b {:c 2 :d {:z 9} :z 3} :e 100}})
+  -> {:a {:b {:z 3, :c 3, :d {:z 9, :x 1, :y 2}}, :e 103}, :f 4}"
+  [f & maps]
+  (apply
+   (fn m [& maps]
+     (if (every? map? maps)
+       (apply merge-with m maps)
+       (apply f maps)))
+   maps))
+
 (defn merge-transpose
   " (foo {:a {:k {:d 1}}}
          {:b {:k {:d 2}}}
