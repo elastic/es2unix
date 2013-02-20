c5698f6e4aac9cdbbbea4dd4b1618615adf825af
Lee Hinman <lee@writequit.org>
2013-02-19 17:52:17 -0700

    Bump dependencies to their latest versions

--- project.clj
+++ project.clj
@@ -6,11 +6,11 @@
   :url "http://elasticsearch.org/"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.5.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.5.0-RC16"]
                  [org.clojure/tools.cli "0.2.2"]
-                 [cheshire "5.0.1"]
-                 [clj-http "0.6.3"]
-                 [log4j/log4j "1.2.16"]
+                 [cheshire "5.0.2"]
+                 [clj-http "0.6.4"]
+                 [log4j/log4j "1.2.17"]
                  [bultitude "0.2.0"]
                  [slingshot "0.10.3"]
                  [com.google.guava/guava "14.0-rc1"]]
